e11f762e4e181326b79df82ef1bc238ffd909682
Andrew Durham <andrewdurham1094@gmail.com>
2021-10-14 18:08:02 -0600

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 // import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
+let sequelize
 
 //to make sure mySQL user info is not on github
 ///I did npm i dotenv and created .env first
@@ -8,10 +9,14 @@ require('dotenv').config();
 //create connection to our database, pass in your MySQL information for username and password
 //just_tech_news_db was already created in db/schema.sql
 //https://sequelize.org/v5/manual/getting-started.html
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    })
+}
 
 module.exports = sequelize;
