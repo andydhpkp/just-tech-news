dc89584f88805c9f64dd659e9cc68fa12d683109
Andrew Durham <andrewdurham1094@gmail.com>
2021-10-18 18:43:32 -0600

    lesson 1

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 //the router instance in routes/index.js collected everything for us and packaged them up for server.js to use
+const path = require('path')
 const express = require('express')
-const routes = require('./routes')
-const sequelize = require('./config/connection')
+//adding express-handlebars
+const exphbs = require('express-handlebars')
 
 const app = express()
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection')
+
+//add the style sheet
+const hbs = exphbs.create({})
+//after this is set up, create the views folder, layout, and the main.handlebars file
+//this combines with exphbs and hbs
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
+//add the style sheet
+app.use(express.static(path.join(__dirname, 'public')))
 
 //turn on routes
-app.use(routes)
+app.use(require('./controllers/')) 
 
 //turn on connection to db and server
 ///the sync part means that this is Sequelize taking the models and connecting them to associated database tables. If it doesn't find a table, it'll create it for you!
 ///force: false doesn't have to be included, but if it were set to true, it would drop and re-create all of the database tables on startup
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'))
-})
+    app.listen(PORT, () => console.log('Now listening'));
+});
+
